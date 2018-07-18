05c16d08e36fd3f0926b4995579dbc2be7801330
Lukasz-GitHub <39419267+Lukasz-GitHub@users.noreply.github.com>
2018-07-18 21:07:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 var fs = require('fs');
 
-fs.readdir('../Zadanie 13.7', function (err, data) {
+fs.readdir('./', function (err, data) {
     var textData = data.join(', ');
-    console.log('Zawartość folderu Zadania 13.7:\n', data);
+    console.log('Zawartość folderu:\n', data);
     fs.writeFile('./Zadanie.txt', textData, function (err) {
         if (err) throw err;
-        console.log('Zawartość folderu Zadania 13.7 została zapisana w pliku Zadanie.txt.\n');
+        console.log('Zawartość folderu została zapisana w pliku Zadanie.txt.\n');
         fs.readFile('./Zadanie.txt', 'utf-8', function (err, data){
             console.log('Zawartość pliku Zadania.txt: ' + data);
         });
